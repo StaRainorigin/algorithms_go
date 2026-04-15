4ae047e25c770db6f69a3891b09c012faa069704
StaRainorigin <1343026891@qq.com>
2026-04-15 10:37:00 +0800

    0003

--- leetcode/0003/solution.go
+++ leetcode/0003/solution.go
@@ -1,22 +1,15 @@
 package solution
 
-func lengthOfLongestSubstring(s string) int {
-	i, j, n := 0, 0, len(s)
-	res := 0
-	aMap := make(map[byte]int)
-	for j < n {
-		idx, ok := aMap[s[j]]
-		if ok {
-			res = max(res, j-i)
-			for i <= idx {
-				delete(aMap, s[i])
-				i++
-			}
-		} else {
-			aMap[s[j]] = j
-			j++
+func lengthOfLongestSubstring(s string) (ans int) {
+	window := [128]bool{}
+	i := 0
+	for j, c := range s {
+		for window[c] {
+			window[s[i]] = false
+			i++
 		}
+		window[c] = true
+		ans = max(ans, j - i + 1)
 	}
-	res = max(res, j-i)
-	return res
+	return
 }
