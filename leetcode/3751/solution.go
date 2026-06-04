92460910d63b411b599b0faf93fb70b2f9defa1e
StaRainorigin <1343026891@qq.com>
2026-06-04 17:23:59 +0800

    3635

---
+++ leetcode/3751/solution.go
@@ -0,0 +1,22 @@
+package solution
+
+import "strconv"
+
+func totalWaviness(num1 int, num2 int) int {
+	ans := 0
+	for x := num1; x <= num2; x++ {
+		s := strconv.Itoa(x)
+		for i := 1; i < len(s)-1; i++ {
+			// 峰
+			if s[i] > s[i-1] && s[i] > s[i+1] {
+				ans++
+			}
+			// 谷
+			if s[i] < s[i-1] && s[i] < s[i+1] {
+				ans++
+			}
+		}
+	}
+	return ans
+
+}
