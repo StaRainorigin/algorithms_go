b98fa1ddc28f44534e363c4f5d4880b69570bca9
景余 <15275257002@163.com>
2026-06-16 08:59:53 +0800

    2265

---
+++ leetcode/0072/2265.go
@@ -0,0 +1,22 @@
+func averageOfSubtree(root *TreeNode) (ans int) {
+	var dfs func(*TreeNode) (int, int)
+	dfs = func(node *TreeNode) (int, int) {
+		sum, cnt := node.Val, 1
+		if node.Left != nil {
+			s, c := dfs(node.Left)
+			sum += s
+			cnt += c
+		}
+		if node.Right != nil {
+			s, c := dfs(node.Right)
+			sum += s
+			cnt += c
+		}
+		if node.Val == sum/cnt {
+			ans++
+		}
+		return sum, cnt
+	}
+	dfs(root)
+	return
+}
